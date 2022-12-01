1d20d5a41796f189fb490488b1e7bf941ab8a508
Michiel Koning <github@michiel.ion3.nl>
2022-12-01 22:51:08 +0100

    Day 01: Calorie Counting

--- elixir/advent2022/lib/day01.ex
+++ elixir/advent2022/lib/day01.ex
@@ -1,5 +1,23 @@
 defmodule Advent2022.Day01 do
-  def solve(_input) do
+  def solve(input) do
+    burdens = input
+    |> read_elves()
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.sort()
+    |> Enum.reverse()
     IO.puts("Part 1:")
+    IO.puts(Enum.fetch!(burdens, 0))
+    IO.puts("Part 2:")
+    IO.puts(burdens |> Enum.take(3) |> Enum.sum())
+  end
+
+  defp read_elves(input) do
+    String.split(input, ~r/\n\n/)
+    |> Enum.map(&read_elf/1)
+  end
+
+  defp read_elf(value) do
+    String.split(value, ~r/\n/)
+    |> Enum.map(&String.to_integer/1)
   end
 end
--- elixir/advent2022/test/day01_test.exs
+++ elixir/advent2022/test/day01_test.exs
@@ -2,7 +2,22 @@ defmodule Advent2022.Day01.Test do
   alias Advent2022.Day01
   use ExUnit.Case
 
+  @sample_input "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"
+
   test "sample input" do
-    assert Day01.solve("") == :ok
+    assert Day01.solve(@sample_input) == :ok
   end
 end
